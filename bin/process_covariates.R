c9ea0681ff1b0b20ba0e6c5b60740341785c42a5
Fnyasimi <nfestus@yahoo.com>
2020-08-13 16:41:59 +0300

    Add linear regression

--- bin/process_covariates.R
+++ bin/process_covariates.R
@@ -5,8 +5,8 @@ argv <- commandArgs(trailingOnly = TRUE)
 # Read in the data
 peer <- argv[1]
 gene_expr <- argv[2]
-outfile <- argv[3]
-
+covs_outfile <- argv[3]
+expr_outfile <- argv[4]
 
 # Read in the data
 peer_factors <- read.csv(file = peer, header = FALSE)
@@ -15,7 +15,22 @@ gene_exp_transpose <- read.csv(file = gene_expr, header = TRUE)
 colnames(peer_factors) <- rownames(gene_exp_transpose)
 
 # write the covariates as a .txt file
-write.table(peer_factors, file = outfile, sep = "\t",
-            row.names = FALSE)
+write.table(peer_factors, file = covs_outfile, sep = "\t",
+            row.names = TRUE)
+
+# linear regression
+# copy the matrix
+expression <- gene_exp_transpose
+
+#Run linear regression on the PEER factor covariates. 
+#Then it sets the residuals to the new expression for that gene.
+for (i in 1:length(colnames(gene_exp_transpose))) {
+  fit = lm(gene_exp_transpose[,i] ~ t(as.matrix(peer_factors)))
+  expression[,i] <- fit$residuals
+}
+
+# Write out the final expression file
+write.table(expression, file = expr_outfile, sep = "\t",
+            row.names = TRUE)
 
 
