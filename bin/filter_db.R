124ae68a6edb83df227a1ad61ef7f079366a5844
Fnyasimi <nfestus@yahoo.com>
2020-08-19 13:15:06 +0300

    Filter the created database

---
+++ bin/filter_db.R
@@ -0,0 +1,34 @@
+#! /usr/bin/env Rscript
+
+suppressMessages(library(dplyr))
+suppressMessages(library(RSQLite))
+
+args <- commandArgs(trailingOnly = TRUE)
+
+
+unfiltered_db <- args[1]
+filtered_db <- args[2]
+
+driver <- dbDriver("SQLite")
+
+in_conn <- dbConnect(driver, unfiltered_db)
+out_conn <- dbConnect(driver, filtered_db)
+model_summaries <- dbGetQuery(in_conn, 'select * from model_summaries where zscore_pval < 0.05 and rho_avg > 0.1')
+model_summaries <- model_summaries %>% 
+	                rename(pred.perf.R2 = rho_avg_squared, genename = gene_name, pred.perf.pval = zscore_pval, n.snps.in.model = n_snps_in_model)
+model_summaries$pred.perf.qval <- NA
+dbWriteTable(out_conn, 'extra', model_summaries, overwrite = TRUE)
+construction <- dbGetQuery(in_conn, 'select * from construction')
+dbWriteTable(out_conn, 'construction', construction, overwrite = TRUE)
+sample_info <- dbGetQuery(in_conn, 'select * from sample_info')
+dbWriteTable(out_conn, 'sample_info', sample_info, overwrite = TRUE)
+weights <- dbGetQuery(in_conn, 'select * from weights')
+weights <- weights %>% filter(gene %in% model_summaries$gene) %>% rename(eff_allele = alt, ref_allele = ref, weight = beta)
+dbWriteTable(out_conn, 'weights', weights, overwrite = TRUE)
+dbExecute(out_conn, "CREATE INDEX weights_rsid ON weights (rsid)")
+dbExecute(out_conn, "CREATE INDEX weights_gene ON weights (gene)")
+dbExecute(out_conn, "CREATE INDEX weights_rsid_gene ON weights (rsid, gene)")
+dbExecute(out_conn, "CREATE INDEX gene_model_summary ON extra (gene)")
+
+dbDisconnect(in_conn)
+dbDisconnect(out_conn)
